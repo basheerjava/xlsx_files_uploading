2aa2ddefc3324699651eb700ec0964b557e2c8ea
basheerjava <basheer.shaik987@gmail.com>
2022-01-06 19:28:33 +0530

    this is the xlsx files uploading project

---
+++ xlsx_file_uploader/src/main/java/com/xls/fileuploader/XlsxFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.xls.fileuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsxFileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XlsxFileUploaderApplication.class, args);
+	}
+
+}
---
+++ xlsx_file_uploader/src/test/java/com/xls/fileuploader/XlsxFileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xls.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlsxFileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
